7ecaa3f9ea7601bb0088c6c96fd69f43b4c1efc1
EC2 Default User <ec2-user@ip-172-31-43-56.ap-northeast-1.compute.internal>
2019-10-05 23:47:07 +0900

    add ma

---
+++ script/gitChangeBranch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# check if execution is with -ex
+if test $1 = '-xe' ;then
+  arg=$2
+else
+  arg=$1
+fi
+
+# change work directory
+cd ~/$arg
+
+# get curent branch
+currentBranch=`git branch --contains=HEAD`
+
+# set change branch
+if test ${currentBranch:2} = 'master' ;then
+  changeBranch=develop
+else
+  changeBranch=master
+fi
+
+# change branch
+git checkout ${changeBranch}
+
+exit 0
